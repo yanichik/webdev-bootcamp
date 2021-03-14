15cdc97c621523396f8552910d1065a42fd997e6
yanichik <brunshte@gmail.com>
2021-03-14 14:32:21 -0700

    example of how middleware inside express Router applies only to that set of routes

--- 47_Express_Router_AND_Cookies/app.js
+++ 47_Express_Router_AND_Cookies/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const dogRoutes = require('./routes/dogRoutes');
+const shelterRoutes = require('./routes/shelterRoutes');
 
 app.use('/dogs', dogRoutes);
+app.use('/shelters', shelterRoutes);
 
 app.listen(3000, ()=>{
 	console.log('Logged into port 3000.');

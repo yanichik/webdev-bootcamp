aee75960f5a7c76f93738a786b52058ad26395d7
yanichik <brunshte@gmail.com>
2021-03-14 22:24:57 -0700

    completed cookie sending, receiving, and parsing exercise

--- 47_Express_Router_AND_Cookies/routes/shelterRoutes.js
+++ 47_Express_Router_AND_Cookies/routes/shelterRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const cookieParser = require('cookie-parser');
+
+router.use(cookieParser());
 
 router.get('/', (req, res) => {
 	res.send('All Shelters');
 })
 
 router.get('/:id', (req, res) => {
-	res.send(`Shelter ID: ${req.params.id}`);
+	const cookies = req.cookies;
+	const keys = Object.keys(cookies);
+	let dogString = '';
+	const allDogs = keys.forEach((key, index) =>{
+		dogString += `${key}: ${cookies[key]}`;
+	})
+	console.log(dogString);
+	res.send(`Shelter ${req.params.id} has ${dogString}`);
 })
 
 router.get('/:id/edit', (req, res) => {

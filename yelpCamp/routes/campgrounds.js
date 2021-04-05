fea0e6fc4d6728e785b4ebd9e1b7422bd30d9f1b
yanichik <brunshte@gmail.com>
2021-04-04 22:48:35 -0700

    yelpCamp: use multer package to upload & parse image/s instead of rendering images from some URL

--- yelpCamp/routes/campgrounds.js
+++ yelpCamp/routes/campgrounds.js
@@ -5,11 +5,16 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const {isLoggedIn, isAuthorLoggedIn, validateCampground} = require('../middleware');
 const campController = require('../controllers/campController');
-
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 // USING this path starter for all paths in this doc: app.use('/campgrounds', campgroundsRoutes);
 router.route('/')
 	.get(catchAsync(campController.renderIndex))
-	.post(isLoggedIn, validateCampground, catchAsync(campController.createNewCamp));
+	// .post(isLoggedIn, validateCampground, catchAsync(campController.createNewCamp));
+	.post(upload.array('image'), (req, res, next) => {
+		console.log(req.body, req.files);
+		res.send("File uploaded.");
+	})
 
 router.get('/new', isLoggedIn, campController.renderNewForm);
 
